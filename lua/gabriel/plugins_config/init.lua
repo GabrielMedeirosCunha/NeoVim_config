dc18744c580f3de3d83c88e775d4441ac7d1150b
Gabriel Medeiros <gabriel13467982@gmail.com>
2023-09-21 11:44:07 -0300

    add comment configs

--- lua/gabriel/plugins_config/init.lua
+++ lua/gabriel/plugins_config/init.lua
@@ -2,4 +2,5 @@ require("gabriel.plugins_config.treesitter_config")
 require("gabriel.plugins_config.telescope_config")
 require("gabriel.plugins_config.LSP_config")
 require("gabriel.plugins_config.nvim-tree_config")
+require("gabriel.plugins_config.comment")
 
